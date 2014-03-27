7fcff83d0b592e9db09fd88adfc69ff26f503632
Aaron Brady <abrady@fb.com>
2014-03-27 13:13:25 -0700

    app groups fetch working

--- cloud/app.js
+++ cloud/app.js
@@ -1,10 +1,44 @@
 var express = require('express');
 var fb = require('cloud/fb.js')
+var fbconfig = require('cloud/fbconfig.js');
 
 // Global app configuration section
 var app = express();
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
+
+// add support for handling multipart/form-data posts
+// ab: not needed unless we want to support file uploads via xmlhttpreqs
+// req is type http.IncomingMessage
+// app.use(function(req, res, next){
+//     if (req.is('multipart/form-data')) {
+//         req.text = '';
+//         req.on('data', function(chunk){ 
+//             //console.log('!!!!!! data chunk: '+chunk);
+//             req.text += chunk
+//         });
+//         req.on('end', function() {
+//             var boundary = req.headers['content-type'];
+//             var delim = boundary.match('boundary=([^;]*);?')[1]
+//             req.body = {}
+//             // each param is bookended by the boundary above, and
+//             // takes the form: 
+//             // <delim>\r\nContent-Disposition: form-data; name:"param_name"\r\n\r\nparam_value\r\n<delim>
+//             // this is just some hacky regexes to take care of that
+//             req.text.split(delim).forEach(function(node,i,a) {           
+//                 m = node.match("name=\"(.*)\"\r\n\r\n(.*)\r\n"); 
+//                 if(m) { 
+//                     req.body[m[1]] = m[2];
+//                 }
+//             });
+//             //console.log('!!!! post:'+JSON.stringify(req.body));
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// });
+// 
 app.use(express.bodyParser());    // Middleware for reading request body
 
 // This is an example of hooking up a request handler with a specific request
@@ -14,28 +48,30 @@ app.get('/hello', function(req, res) {
 });
 
 index = function(req, res) {
-    console.log('getting access token');
-    fb.appAccessTokenRequest(function(app_access_token) {
-        console.log('got access token');
-        res.render('index', { app_access_token: app_access_token });
-    });
+  console.log('index');
+  fb.groups.getAll(function(app_groups) {
+    console.log('got groups'+JSON.stringify(app_groups));
+    res.render('index', { app_id: fbconfig.app_id, app_groups: app_groups })
+  });
 }
 app.post('/', index);
 app.get('/', index);
 
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
-
-// app.use(express.static('public'));
+app.post('/groupcreate', function(req, res) {
+    console.log('groupcreate request body:'+JSON.stringify(req.body));
+    fb.groupCreate(
+        req.body.name, 
+        req.body.description, 
+        req.body.privacy, 
+        null, 
+        function (create_res) {
+            res.end(JSON.stringify({"result":create_res}));
+        },
+        function (err) {
+            res.end(JSON.stringify({"error":err}));
+        }
+    );             
+});
 
 // Attach the Express app to Cloud Code.
 app.listen();
--- public/group.js
+++ public/group.js
@@ -1,1 +1,135 @@
+"use strict";
 
+var ge = function(e) {
+  return typeof e == 'string' ? document.getElementById(e) : e;
+};
+var $ = function(args) {
+  var e = ge.apply(this, arguments);
+  if (!e) {
+    throw new Error('Tried to get element '+args+'but it is not present in the page. Use ge() instead.');
+  }
+  return e;
+}
+
+// generic command sender, response is logged
+// res is of form([true,false],response text)
+function server_send_cmd(cmd,args, res) {
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function()
+  {
+    if (xmlhttp.readyState==4)
+    {
+      console.log(xmlhttp.status +', '+ xmlhttp.responseText);
+      if (res) {
+        res(xmlhttp.status==200, xmlhttp.responseText)
+      }
+    }
+  }
+
+  xmlhttp.open("GET",cmd+'?res='+escape(args),true);
+  xmlhttp.send();
+}
+
+function permissions_validate(perms_string, response) {
+  var missing = '';
+  var perms = perms_string.split(',');
+  for(var i = 0; i < perms.length; ++i) {
+    var k = perms[i];
+    if (-1 == response.perms.indexOf(k))
+      missing += (missing?', ':'')+k;
+  }
+  if(missing) {
+    console.log('missing permissions: ' + missing);
+    return false;
+  }
+  return true;
+}
+
+// log the user in/ask permissions
+// note: on_logged_in() takes further actions
+function onFBInit() {
+  var permissions = ''; // 'publish_stream,publish_actions,user_games_activity';
+  FB.login(
+      function(response) {
+          if(response.authResponse) {
+              // logged in
+              onLogin();
+            }},
+      {scope: permissions}
+    );
+}
+
+function onLogin() {
+  // console.log('access_token: ' + FB.getAccessToken());
+  //  FB.api('/me', user_recv);
+  getUserGroups(printUserGroups);
+}
+
+function printUserGroups(groups) {
+  var node = $('player_groups');
+  console.log('groups res'+JSON.stringify(groups));
+}
+
+function getUserGroups(res_cb) {
+  fbGraphGet(
+    '/me/groups?parent='+fbconfig.app_id+'&access_token='+FB.getAccessToken(), 
+    res_cb
+  );
+}
+
+function fbGraphGet(path, res_cb, err_cb) {
+  if (path && path[0] != '/') {
+    path = '/' + path;
+  }
+  var xhr = new XMLHttpRequest();
+  console.log("get user groups");
+  xhr.open(
+    'get', 
+    'https://graph.facebook.com'+path,
+    true
+  );
+  xhr.onreadystatechange=function()
+  {
+    if (xhr.readyState==4)
+    {
+      if(xhr.status==200) {
+        if (res_cb) {
+          res_cb(JSON.parse(xhr.responseText))
+        }
+      } else {
+        if (err_cb) {
+          err_cb(xhr.status, xhr.responseText);
+        } else {
+          console.error('request failed: ' +path);
+        }         
+      }
+    }
+  }
+  xhr.send();
+}
+
+function onGroupCreateRes(res_str) {
+  // {"result":"{\"id\":\"1488155834741485\"}"}
+  res_json = JSON.parse(res_str);
+  console.log("result: "+res_json.result);
+}
+
+function submitForm(form_elt, res)
+{
+  var xhr = new XMLHttpRequest();
+  console.log("submitting, method: "+form_elt.method+" action: "+form_elt.action);
+  xhr.onload = res;
+  xhr.open(form_elt.method, form_elt.action, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  //    xhr.send(new FormData (form_elt)); <= screw this. sends as multipart/form-data
+  //http://www.w3.org/TR/2010/WD-XMLHttpRequest2-20100907/#dom-xmlhttprequest-send
+  form_fields = {};
+  for(i=0; i<form_elt.elements.length; i++)
+  {
+    if (form_elt.elements[i].name) {
+      form_fields[form_elt.elements[i].name] = form_elt.elements[i].value;
+    }
+  }
+  xhr.send(JSON.stringify(form_fields));
+  return false; // signal that the submit was handled
+}
